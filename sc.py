3283fef99f6fc675d781ea50de0a610a68c190e6
bharathani20 <42990343+bharathani20@users.noreply.github.com>
2018-10-10 10:31:57 +0530

    Add files via upload

--- sc.py
+++ sc.py
@@ -6,18 +6,18 @@ o=input("what do you want to do?,+,-,/,*: ")
 
 def add():
     return i+j
- def sub(a,b):
+def sub(a,b):
     return a-b
-  def mult(a,b):     
+def mult(a,b):     
     return a*b
-  def div(a,b):
+def div(a,b):
     return a/b
-  if (o=='+'):
-  	    print("addition=", add())
-  elif (o=='-'):
+if (o=='+'):
+  	  print("addition=", add())
+elif (o=='-'):
    		print("subtraction=", sub(50,90))
-   elif (o=='*'):
-        print("multipication=", mult())
-   elif (o=='/'): 
-         print("divison=", div())     
+elif (o=='*'):
+      print("multipication=", mult())
+elif (o=='/'): 
+      print("divison=", div())     
             	
