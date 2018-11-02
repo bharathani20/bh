37c2dea49b54d64eb40dcd4ec9362b5b60d060cd
bharathani20 <42990343+bharathani20@users.noreply.github.com>
2018-11-02 10:29:05 +0530

    Add files via upload

---
+++ graph5.py
@@ -0,0 +1,26 @@
+#graph
+import matplotlib.pyplot as plt
+import csv
+
+x=[]
+y=[]
+z=[]
+
+with open('graph.csv', 'r') as csvfile:
+    plots= csv.reader(csvfile, delimiter=',')
+    for row in plots:
+        x.append(int(row[0]))
+        y.append(int(row[1]))
+        z.append(int(row[2]))
+
+
+plt.plot(x,y, marker='o')
+plt.plot(x,z, marker='o')
+
+plt.title('Data from the CSV File: People and Expenses')
+
+plt.xlabel('Number of People')
+plt.ylabel('Expenses')
+
+plt.show()
+
