ab99599a0504b1e2293b39aca7611ca4adbf2e5e
bharathani20 <42990343+bharathani20@users.noreply.github.com>
2018-11-30 13:43:57 +0530

    Add files via upload

---
+++ p2.py
@@ -0,0 +1,11 @@
+import os
+import pytesseract
+import gtts
+import image
+
+def image():
+	os.system(home/cl218/bharat/+"1.jpg")
+lang = "english"
+spell = gtts.gTTS("hello")
+spell.save("welcome.mp3")
+os.system("mpg321 welcome.mp3")
